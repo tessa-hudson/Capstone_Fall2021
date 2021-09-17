46a2aad63cc0bf5dd72ea0bd2c5a1f39d22cc0d5
Zach Bender <zfbender@crimson.ua.edu>
2021-09-17 11:32:55 -0500

    Basic landing page set up w/ no styling; Added basic form for adding a camper and a group

---
+++ score-tracker-frontend/src/AddCamperForm.jsx
@@ -0,0 +1,52 @@
+import React, { Component } from 'react'
+import './AddCamperForm.css'
+
+class AddCamperForm extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {firstName: '', lastNameInitial: ''}
+
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    handleChange(event) {
+        const target = event.target
+        const value = target.value
+        const name = target.name
+        
+        this.setState({
+            [name]: value
+        })
+    }
+
+    handleSubmit(event) {
+        event.preventDefault() //This prevents the page from refreshing on submit
+        console.log(`First: ${this.state.firstName}, Last: ${this.state.lastNameInitial}`)
+    }
+
+    render() {
+        return (
+            <div className="AddCamper">
+                <h3>Use this form to add a camper!</h3>
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        First Name:
+                        <br />
+                        <input type="text" name="firstName" value={this.state.firstName} onChange={this.handleChange} />
+                    </label>
+                    <br />
+                    <label>
+                        First Initial of Last Name:
+                        <br />
+                        <input type="text" name="lastNameInitial" value={this.state.lastNameInitial} onChange={this.handleChange} />
+                    </label>
+                    <br />
+                    <input type="submit" value="Submit" />
+                </form>
+            </div>
+        )
+    }
+}
+
+export default AddCamperForm
---
+++ score-tracker-frontend/src/AddGroupForm.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import './AddGroupForm.css'
+
+class AddGroupForm extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {groupName: ''}
+
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    handleChange(event) {
+        this.setState({groupName: event.target.value})
+    }
+
+    handleSubmit(event) {
+        event.preventDefault() //This prevents the page from refreshing on submit
+        console.log(`Group Name: ${this.state.groupName}`)
+    }
+
+    render() {
+        return (
+            <div className="AddGroup">
+                <h3>Use this form to add a new group!</h3>
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        Group Name:
+                        <br />
+                        <input type="text" value={this.state.groupName} onChange={this.handleChange} />
+                    </label>
+                    <br />
+                    <input type="submit" value="Submit" />
+                </form>
+            </div>
+        )
+    }
+}
+
+export default AddGroupForm
