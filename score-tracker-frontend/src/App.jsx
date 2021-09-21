04252e9a10807652f4df61c764075ef0679d72d5
Zach Bender <zfbender@crimson.ua.edu>
2021-09-21 00:43:57 -0500

    Added in routing and moved the forms to their own pages

--- score-tracker-frontend/src/App.jsx
+++ score-tracker-frontend/src/App.jsx
@@ -1,16 +1,25 @@
 import React, { useState } from 'react'
+import { Route, Switch, NavLink} from "react-router-dom"
 import './App.css'
 import AddCamperForm from './AddCamperForm'
 import AddGroupForm from './AddGroupForm'
+import HomePage from './HomePage'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <h1>Welcome to the Camp Clot Not Scorekeeper App!</h1>
-      <AddCamperForm />
-      <AddGroupForm />
+      <nav className="App-nav">
+        <NavLink exact activeClassName="active-link" to="/">Home</NavLink>
+        <NavLink exact activeClassName="active-link" to="addCamper">Add Camper</NavLink>
+        <NavLink exact activeClassName="active-link" to="addGroup">Add Group</NavLink>
+      </nav>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/addCamper" component={AddCamperForm} />
+        <Route exact path="/addGroup" component={AddGroupForm} />
+      </Switch>
     </div>
   )
 }
---
+++ score-tracker-frontend/src/HomePage.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import './HomePage.css'
+
+class HomePage extends Component {
+    render() {
+        return (
+            <div className="Home">
+                <h1>Welcome to the Camp Clot Knot Score Keeper!</h1>
+                <h3>See the ScoreBoard Below!</h3>
+            </div>
+        )
+    }
+}
+
+export default HomePage
