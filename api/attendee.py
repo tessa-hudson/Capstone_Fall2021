614b6d8876388904d6a43fa1d04da7dad80bc9bc
Tessa Tryon <78631551+tessa-nine@users.noreply.github.com>
2021-09-23 13:27:21 -0500

    refine the attendee resource (prev camper)

---
+++ api/attendee.py
@@ -0,0 +1,73 @@
+from flask import request
+from marshmallow import Schema, fields, post_load, ValidationError
+from flask_restful import abort, Resource, Api
+
+class Attendee():
+    def __init__(self, firstname, last_initial, id = -1):
+        self.id = id
+        self.firstname = firstname
+        self.last_initial = last_initial
+
+    def __repr__(self):
+        return "<Attendee(id={self.id}, firstname={self.firstname!r}, last_initial={self.last_initial!r})>".format(self=self)
+
+class AttendeeSchema(Schema):
+    id = fields.Int() #change to UUID eventually
+    firstname = fields.Str(required=True)
+    last_initial = fields.Str(required=True)
+
+    @post_load
+    def make_attendee(self, data, **kwargs):
+        return Attendee(**data)
+
+attendee_schema = AttendeeSchema()
+attendees_schema = AttendeeSchema(many=True)
+
+attendees = [
+    Attendee("fname", "li", 1),
+    Attendee("fname2", "li2", 2),
+    Attendee("fname3", "li3", 3)
+]
+
+def search(id):
+    return [a for a in attendees if a.id == id]
+        
+def get_attendee(id):
+    comp = search(int(id))
+    if not comp:
+        abort(404, message="Camper {} doesn't exist".format(id))
+    else:
+        return comp[0]
+
+# shows a single attendee and lets you delete an attendee
+class AttendeeResource(Resource):
+
+    def get(self, id):
+        attendee = get_attendee(id)
+        result = attendee_schema.dump(attendee)
+        return result
+    
+    def delete(self, id):
+        attendee = get_attendee(id)
+        attendees.remove(attendee)
+        return '', 204
+
+# shows a list of all campers and lets you POST to add new attendees
+class AttendeeListResource(Resource):
+    
+    def get(self):
+            result = attendees_schema.dump(attendees)
+            return {"attendees": result}
+
+    def post(self):
+        data = request.get_json()
+        if not data:
+            return {"message": "No input data provided"}, 400
+        try:
+            new_attendee = attendee_schema.load(data)
+            new_attendee.id = len(attendees) + 1
+            attendees.append(new_attendee)
+        except ValidationError as err:
+            return err.messages, 422
+        print(attendees)
+        return attendee_schema.dump(new_attendee), 201
--- api/router.py
+++ api/router.py
@@ -7,8 +7,8 @@ api = Api(app)
 ##
 ## Actually setup the Api resource routing here
 ##
-api.add_resource(CamperList, '/campers')
-api.add_resource(Camper, '/campers/<camper_id>')
+api.add_resource(AttendeeListResource, '/attendees')
+api.add_resource(AttendeeResource, '/attendees/<id>')
 
 if __name__ == '__main__':
     app.run(debug=False)
