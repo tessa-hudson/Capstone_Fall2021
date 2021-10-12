2ab9c737f5b53669ed277ca619de3ccf176be0a1
Zach Bender <zfbender@crimson.ua.edu>
2021-10-12 14:39:24 -0500

    addition of auth0 to project

--- score-tracker-frontend/src/App.jsx
+++ score-tracker-frontend/src/App.jsx
@@ -1,26 +1,27 @@
 import React, { useState } from 'react'
 import { Route, Switch, NavLink} from "react-router-dom"
 import './App.css'
-import AddCamperForm from './Components/Forms/AddCamperForm'
-import AddGroupForm from './Components/Forms/AddGroupForm'
 import HomePage from './Components/HomePage'
-import Groups from './Components/Groups.jsx'
-import Campers from './Components/Campers.jsx'
+import Groups from './Components/Groups'
+import Campers from './Components/Campers'
+import NavBar from './Components/Navbar'
+import Profile from './Components/Profile'
+import LandingPage from './Components/LandingPage'
+import ProtectedRoute from './auth/ProtectedRoute'
 
 function App() {
+
   return (
     <div className="App">
-      <div className="Nav">
-        <nav className="App-nav">
-          <NavLink exact activeClassName="active-link" to="/">Home</NavLink>
-          <NavLink exact activeClassName="active-link" to="/groups">Groups</NavLink>
-          <NavLink exact activeClassName="active-link" to="/campers">Campers</NavLink>
-        </nav>
-      </div>
+      {
+        location.pathname !== '/' && <NavBar />
+      }
       <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="/groups" component={Groups} />
-        <Route exact path="/campers" component={Campers} />
+        <Route exact path="/" component={LandingPage} />
+        <ProtectedRoute exact path="/home" component={HomePage} />
+        <ProtectedRoute exact path="/groups" component={Groups} />
+        <ProtectedRoute exact path="/campers" component={Campers} />
+        <ProtectedRoute exact path="/profile" component={Profile} />
       </Switch>
     </div>
   )
