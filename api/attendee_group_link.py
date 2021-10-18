7e8a398c11a3455a2b5c5c1f47069f9c57a20f34
Tessa Tryon <78631551+tessa-nine@users.noreply.github.com>
2021-10-18 10:50:27 -0500

    match up variables

--- api/attendee_group_link.py
+++ api/attendee_group_link.py
@@ -7,27 +7,31 @@ from connection import conn
 
 # AttendeeGroupLink class
 class AttendeeGroupLink():
-    def __init__(self, event_id, attendee_group_link_name, attendee_group_link_id = -1, total_points = 0):
-        self.attendee_group_link_id = attendee_group_link_id
-        self.event_id = event_id
-        self.attendee_group_link_name = attendee_group_link_name
+    def __init__(self, attendee_id, event_id, group_id, attendee_group_link_id = -1, total_points = 0):
         self.total_points = total_points
+        self.attendee_group_link_id = attendee_group_link_id
+        self.attendee_id = attendee_id
+        self.event_id = event_id
+        self.group_id = group_id
+        
 
     def __repr__(self):
-        return "<AttendeeGroupLink(attendee_group_link_id={self.attendee_group_link_id}, event_id={self.event_id} attendee_group_link_name={self.attendee_group_link_name!r}, total_points={self.total_points})>".format(self=self)
+        return "<AttendeeGroupLink(total_points={self.total_points}, attendee_group_link_id={self.attendee_group_link_id}, attendee_id={self.attendee_id}, event_id={self.event_id}, group_id={self.group_id})>".format(self=self)
 
 # Marshmallow Schema for AttendeeGroupLink
 class AttendeeGroupLinkSchema(Schema):
-    attendee_group_link_id = fields.UUID() # generated when POST request is recieved
-    event_id = fields.UUID(required=True) # event that the attendee_group_link is participating in
-    attendee_group_link_name = fields.Str(required=True) # must be included in POST request
-    total_points = fields.Int() # defaults to 0 when 
+   total_points = fields.Int() # defaults to 0 when AttendeeGroupLink is made
+   attendee_group_link_id = fields.UUID() # generated when POST request is recieved
+   attendee_id = fields.UUID(required=True) # attendee being added to group
+   event_id = fields.UUID(required=True) # event that the group is participating in
+   group_id = fields.UUID(required=True) # group attenddee is being added to 
+    
 
 
-    # Once POST request has been validated deserialized, make a new AttendeeGroupLink with data
-    @post_load
-    def make_attendee_group_link(self, data, **kwargs):
-        return AttendeeGroupLink(**data)
+   # Once POST request has been validated deserialized, make a new AttendeeGroupLink with data
+   @post_load
+   def make_attendee_group_link(self, data, **kwargs):
+      return AttendeeGroupLink(**data)
 
 # Schema to use when loading/dumping a single AttendeeGroupLink
 attendee_group_link_schema = AttendeeGroupLinkSchema()
@@ -75,7 +79,13 @@ class AttendeeGroupLinkListResource(Resource):
             new_attendee_group_link = attendee_group_link_schema.load(data)
             print(data)
             new_attendee_group_link.attendee_group_link_id = uuid.uuid4()
-            conn.add_attendee_group_link(new_attendee_group_link.attendee_group_link_id, new_attendee_group_link.event_id, new_attendee_group_link.attendee_group_link_name, new_attendee_group_link.total_points)
+            conn.add_attendee_group_link(
+               new_attendee_group_link.total_points,
+               new_attendee_group_link.attendee_group_link_id,
+               new_attendee_group_link.attendee_id,
+               new_attendee_group_link.event_id,
+               new_attendee_group_link.group_id
+            )
         except ValidationError as err:
             print(data)
             return err.messages, 422
