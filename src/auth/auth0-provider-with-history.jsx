97119b2dd57fe458bbff4da1e2566c5f672418c9
Tessa Tryon <78631551+tessa-nine@users.noreply.github.com>
2021-10-25 21:51:23 -0500

    change redirect

--- src/auth/auth0-provider-with-history.jsx
+++ src/auth/auth0-provider-with-history.jsx
@@ -17,7 +17,7 @@ const Auth0ProviderWithHistory = ({ children }) => {
       domain={domain}
       clientId={clientId}
       //redirectUri={window.location.origin}
-      redirectUri={"https://hbdatracking.azurewebsites.net"}
+      redirectUri={"http://localhost:3000"}
       onRedirectCallback={onRedirectCallback}
     >
       {children}
