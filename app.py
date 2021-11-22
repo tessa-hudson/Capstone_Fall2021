82b1744d91e977e51c00b4f1362326f5a923aabf
Tessa Tryon <78631551+tessa-nine@users.noreply.github.com>
2021-11-22 13:52:30 -0600

    move route registration

--- app.py
+++ app.py
@@ -4,15 +4,15 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app, support_credentials=True)
 
+from api.attendee import attendeebp
+from api.event import eventbp
+from api.group import groupbp
+
+app.register_blueprint(attendeebp)
+app.register_blueprint(eventbp)
+app.register_blueprint(groupbp)
+
 if __name__ == '__main__':
-    from api.attendee import attendeebp
-    from api.event import eventbp
-    from api.group import groupbp
-
-    app.register_blueprint(attendeebp)
-    app.register_blueprint(eventbp)
-    app.register_blueprint(groupbp)
-
     app.run()
     
     
