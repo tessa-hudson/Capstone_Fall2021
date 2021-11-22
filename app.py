8b3b899d45d7a185f981203cee8a0a661056b488
Tessa Tryon <78631551+tessa-nine@users.noreply.github.com>
2021-11-22 13:34:59 -0600

    rename APP to app

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
 
-APP = Flask(__name__)
+app = Flask(__name__)
 CORS(APP, support_credentials=True)
 
 if __name__ == '__main__':
@@ -9,11 +9,11 @@ if __name__ == '__main__':
     from api.event import eventbp
     from api.group import groupbp
 
-    APP.register_blueprint(attendeebp)
-    APP.register_blueprint(eventbp)
-    APP.register_blueprint(groupbp)
+    app.register_blueprint(attendeebp)
+    app.register_blueprint(eventbp)
+    app.register_blueprint(groupbp)
 
-    APP.run()
+    app.run()
     
     
     
