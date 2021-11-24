b302d948d6ba53794030e1aaf19830d470ba117e
Tessa (Tryon) Hudson <78631551+tessa-hudson@users.noreply.github.com>
2021-11-23 19:56:41 -0600

    Tessa/unit testing attendees (#83)
    * remove keep_alive and server data
    
    * attendees auth
    
    * getAttendees authentication
    
    * add auth for attendee creation
    
    * auth for attendee deletion
    
    * auth for attendee update
    
    * Use env variables
    
    * auth for groups
    
    * events auth
    
    * fix importerror
    
    * fix update
    
    * change .env for prod
    
    * first unit test
    
    * add tests and action
    
    * Update branches_backend_build_check.yml
    
    * print drivers
    
    * print conn
    
    * Update branches_backend_build_check.yml
    
    * Update branches_backend_build_check.yml
    
    * Update branches_backend_build_check.yml
    
    * revert connection.py changes
    
    * add attendee tests
    
    * add unit tests to workflow
    
    Co-authored-by: Tessa Tryon <78631551+tessa-nine@users.noreply.github.com>

---
+++ api/tests/api_test.py
@@ -0,0 +1,28 @@
+from flask import Flask
+import unittest
+
+from api.handlers import handlerbp
+from api.attendee import attendeebp
+from api.event import eventbp
+from api.group import groupbp
+
+from api.Conns.connection import conn
+
+class ApiTest(unittest.TestCase):
+   @classmethod
+   def setUpClass(self):
+      APP = Flask(__name__)
+      APP.testing = True
+      APP.register_blueprint(handlerbp)
+      APP.register_blueprint(attendeebp)
+      APP.register_blueprint(eventbp)
+      APP.register_blueprint(groupbp)
+
+      self.app = APP.test_client()
+
+      conn.delete_all_attendees()
+
+if __name__ == '__main__':
+   unittest.main()
+
+
--- app.py
+++ app.py
@@ -4,10 +4,12 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app, support_credentials=True)
 
+from api.handlers import handlerbp
 from api.attendee import attendeebp
 from api.event import eventbp
 from api.group import groupbp
 
+app.register_blueprint(handlerbp)
 app.register_blueprint(attendeebp)
 app.register_blueprint(eventbp)
 app.register_blueprint(groupbp)
